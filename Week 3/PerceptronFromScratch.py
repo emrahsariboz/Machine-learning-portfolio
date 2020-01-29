a4ed0c71ace1b78b6cc95426645ba8fce76c562c
Emrah <emrahsarboz@gmail.com>
2020-01-28 21:34:52 -0700

    visualization of dataset'

--- Week 3/PerceptronFromScratch.py
+++ Week 3/PerceptronFromScratch.py
@@ -4,12 +4,13 @@ Created on Fri Jan 24 05:27:51 2020
 
 @author: EmrahSariboz
 """
-
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Perceptron():
     
-    def __init__(self, learningRate, n_iter = 50, rnd_state = 1):
+    def __init__(self, learningRate = 0.01, n_iter = 50, rnd_state = 1):
         self.learningRate = learningRate
         self.n_iter = n_iter
         self.rnd_state = rnd_state
@@ -44,7 +45,25 @@ class Perceptron():
     
     def predict(self, X):
         return np.where(self.net_input(X)>=0.0, 1, -1)
-    
+
+
+
+df = pd.read_csv("irishDataset.csv")
+
+y = df.iloc[0:100, 4].values
+
+y = np.where(y == 'setosa', -1, 1)
+
+X= df.iloc[0:100, [0,2]].values
+
+plt.scatter(X[:50, 0], X[:50, 1],
+            color = 'red', marker='o', label='setosa')
         
-        
     
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color = 'blue', marker='x', label='versicolor')
+
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc = 'upper left')
+plt.show()
