833f7e53b2a2dca07d3d4f42aff965969945f8b9
Emrah <emrahsarboz@gmail.com>
2020-02-11 23:31:07 -0700

    Week 6

--- Week 4/LinearRegressionWithGradientDescent.py
+++ Week 4/LinearRegressionWithGradientDescent.py
@@ -1,82 +1,75 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb  2 18:46:18 2020
-
-@author: EmrahSariboz
-"""
-
 import numpy as np
-from sklearn.datasets import load_boston
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-import sympy as sym
+from sklearn.datasets import load_boston
+import pandas as pd
+
+
+df = pd.read_csv("C:/Users/Emrah Sariboz/GitHubProjects/Machine-learning-portfolio/Week 5/irishDataset.csv")
 
 
 
-
-# =============================================================================
-# X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target)
-# 
-# 
-# clf = LinearRegression()
-# clf.fit(X_train, y_train)
-# 
-# 
-# 
-# predicted = clf.predict(X_test)
-# expected = y_test
-# plt.figure(figsize=(4, 3))
-# plt.scatter(expected, predicted)
-# plt.axis('tight')
-# plt.xlabel('True price ($1000s)')
-# plt.ylabel('Predicted price ($1000s)')
-# plt.tight_layout()
-# 
-# MSE =(1 / (2 * len(dataset.target)))* np.sum((predicted-expected)** 2)
-# print('MSE : ', MSE)
-# 
-# =============================================================================
-
-X= [2,4,5]
-y = [1.2, 2.8, 5.3]
-
-X = np.array(X).reshape(-1,1)
-y = np.array(y).reshape(-1,1)
-lm = LinearRegression()
-lm.fit(X, y)
+X = df.iloc[0:100, [0,2]].values
+y = df.iloc[0:100, 4].values
+y = np.where(y == "setosa", -1, 1)
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
 
 
-
-cost = b0 = 0
-b1 = 1
-error = []
-lr = 0.001
-
-for i in range(10000):
-    y_pred = (b0 + b1*X)
-    cost = (y - y_pred)**2
-    partial_b0 = -2 * (y - (b0 + b1 * X))
-    partial_b1 = (-2 * X) * (y - (b0 + b1 * X))
-    b0 = b0 - lr * partial_b0.sum()
-    b1 = b1 - lr * partial_b1.sum()
-    error.append(cost.sum())
+class AdalineSGD():
     
+    def __init__(self, eta = 0.01, n_iter = 10, shuffle = True, random_state = None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        self.random_state = random_state
+    
+    def fit(self, X, y):
+        
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc = 0.0, scale = 0.01, size = 1 + X.shape[1])
+        
+        self.cost_ = []
+        
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X,y = self._shuffle(X,y)
+            cost = []
+            
+            for xi, yi in zip(X, y):
+                cost.append(self._update_weights(xi, yi))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
+        return self
+    
+    def _shuffle(self, X, y):
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+    
+    def _update_weights(self, xi, yi):
+        
+        output = self.net_input(xi)
+        
+        error = (yi - output)
+        cost = (1/2) * (error **2)
+        
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+     
+        return cost
+    
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def predict(self, X):
+        if self.net_input(X) >= 0.0 :
+            return 1
+        return -1
 
-y_pred = lm.predict(X)
-plt.scatter(X, y, color = 'r')
-plt.plot(X, y_pred)
-
-# =============================================================================
-# plt.figure(figsize=(12, 7))
-# plt.title('Gradient Descent - error')
-# plt.plot(np.arange(1, len(error) + 1), error)
-# plt.show()
-# =============================================================================
-
-print('B0 ' , b0, 'B1 ', b1)
-prediction = b0 + b1 * 7.2
-
-
-
+ada = AdalineSGD(n_iter = 15, eta= 0.01, random_state=1)
+ada.fit(X_std, y)
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker = 'o')
+plt.xlabel("Epoch")
+plt.ylabel("Average Cost")
+plt.show()
