af8c7bd373d9f69ff11c7996538d4ec356af34cb
Emrah <emrahsarboz@gmail.com>
2020-02-16 21:54:17 -0700

    Backward Elimination

--- Week 6/MultipleLinearRegression.py
+++ Week 6/MultipleLinearRegression.py
@@ -9,7 +9,6 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import numpy as np
 
@@ -37,7 +36,43 @@ regressor.fit(X_train, y_train)
 #Predicting the test set results
 y_prediction = regressor.predict(X_test)
 
+print("Before backward elimination" , regressor.score(X_test,y_test))
+
+
+
 
 #Building the optimal model using Backward Elimination
+#Backward Elimination
 import statsmodels.formula.api as sm
 X = np.append(arr = np.ones((50, 1)).astype(int), values = X , axis = 1)
+X_optimal = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_optimal).fit()
+
+
+
+
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y,x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary() 
+    return x
+    
+
+SL = 0.05
+X_optimal = X[:, [0, 1, 2, 3, 4, 5]]
+X_modeled = backwardElimination(X_optimal, SL)
+
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting the test set results
+y_prediction = regressor.predict(X_test)
+
+print("After backward elimination" , regressor.score(X_test,y_test))
