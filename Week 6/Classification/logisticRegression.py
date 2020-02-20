be0a008af44d0eab4a3f83a113a5fb19636db88a
Emrah <emrahsarboz@gmail.com>
2020-02-20 11:22:55 -0700

    Logistic Regression

--- Week 6/Classification/logisticRegression.py
+++ Week 6/Classification/logisticRegression.py
@@ -41,3 +41,42 @@ y_prediction = lg.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_prediction)
 
+#Visualization of the training set results
+
+
+
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = X_train, y_train
+
+X1, X2 = np.meshgrid(np.arange(X_set[:,0].min() -1 , X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange (X_set[:, 1].min() - 1, X_set[:, 1].max() +1 , step = 0.01))
+
+
+
+plt.xlim(X1.min(), X1.max())
+plt.ylabel(X2.min(), X2.max())
+
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set==j, 1], 
+                c = ListedColormap(('red', 'green'))(i), label =j)
+    
+
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
