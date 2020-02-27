f5d07d6df3079f9a54d70bbae6a301fba760df93
Emrah <emrahsarboz@gmail.com>
2020-02-26 23:38:48 -0700

    SVM

---
+++ Week 7/Support Vector Machine/SVM.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 23:37:07 2020
+
+@author: EmrahSariboz
+"""
+
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv("Social_Network_Ads.csv")
+
