d1a0c329e8a058febed48ab290b169aa11b8aa40
Emrah <emrahsarboz@gmail.com>
2020-02-29 10:42:59 -0700

    RBF SVM

--- Week 7/Kernel SVM/SVM.py
+++ Week 7/Kernel SVM/SVM.py
@@ -24,9 +24,10 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+
 ##Classifier
 from sklearn.svm import SVC
-classifier = SVC(kernel= 'linear', random_state = 0)
+classifier = SVC(kernel= 'rbf', random_state = 0)
 
 classifier.fit(X_train,y_train)
 
@@ -61,6 +62,29 @@ plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
 
+#Visualizing the training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+
+X1, X2 = np.meshgrid(np.arange(X_set[:,0].min() -1 , X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange (X_set[:, 1].min() - 1, X_set[:, 1].max() +1 , step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
+        alpha = 0.75, cmap = ListedColormap(('red','green')))
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set==j, 1], 
+                c = ListedColormap(('red', 'green'))(i), label =j)
+    
+
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
 
 
 
